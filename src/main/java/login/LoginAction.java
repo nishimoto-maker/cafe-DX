790bd62393bd8ba546993a67c3d3bee862479feb
hrs-2521412 <hrs2521412@stu.o-hara.ac.jp>
2026-07-09 12:19:01 +0900

    a

--- src/main/java/login/LoginAction.java
+++ src/main/java/login/LoginAction.java
@@ -45,11 +45,11 @@ public class LoginAction extends Action {
             
             // ログイン名が管理者(admin)の場合
             if ("admin".equals(staff.getStaff_id())) {
-                return "/kanri/menu2.jsp";
+                return "/order/#";
             }
 
             // ログイン成功ページへ遷移
-            return "/kanri/menu.jsp";
+            return "/order/menu_create.jsp";
         } else {
         	// ログイン名とパスワードが一致しない場合(ログイン失敗)
         	// ユーザーが見つからなかった場合(ログイン失敗)
