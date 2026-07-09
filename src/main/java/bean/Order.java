9d0baade2dffe4e152e3374a66f72a68931f375a
nishimoto-maker <nishinish6121@gmail.com>
2026-07-09 16:19:06 +0900

    Orderbean作成

---
+++ src/main/java/bean/Order.java
@@ -0,0 +1,44 @@
+package bean;
+
+import java.sql.Date;
+
+public class Order implements java.io.Serializable {
+	private int order_id;
+	private String tableNum;
+	private Date ordered_at;
+	private String menu_id;
+	private int count;
+	private boolean payment_flg;
+	
+	public int getOrderId() {
+		return order_id;
+	}
+	public String getTableNum() {
+		return tableNum;
+	}
+	public Date getOrderedAt() {
+		return ordered_at;
+	}
+	public String getMenuId() {
+		return menu_id;
+	}
+	public int getCount() {
+		return count;
+	}
+	public boolean getPaymentFlg() {
+		return payment_flg;
+	}
+	
+	public void setTableNum(String tableNum) {
+		this.tableNum = tableNum;
+	}
+	public void setMenuId(String menu_id) {
+		this.menu_id = menu_id;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public void setPaymentFlg(boolean payment_flg) {
+		this.payment_flg = payment_flg;
+	}
+}
