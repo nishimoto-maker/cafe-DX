6d9fa8dad14b9a77ca6d7780f0e5dce9bfbf0975
nishimoto-maker <nishinish6121@gmail.com>
2026-07-13 12:16:50 +0900

    カートに追加するサーブレット

--- src/main/java/order/CartAddAction.java
+++ src/main/java/order/CartAddAction.java
@@ -9,3 +9,41 @@ order単位で格納していく
 book4のカートまわりを参照する
 
 遷移先：order_cart.jsp*/
+
+package order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.Order;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+
+public class CartAddAction extends Action {
+	@SuppressWarnings("unchecked")
+	public String execute(HttpServletRequest req, HttpServletResponse resp)
+	throws Exception {
+		HttpSession session = req.getSession();
+		
+		String menu_id = req.getParameter("menu_id");
+		int count = Integer.parseInt(req.getParameter("count"));
+		String tableNum = req.getParameter("tabelNum");
+		
+		List<Order> cart = (List<Order>)session.getAttribute("cart");
+		if (cart == null) {
+			cart = new ArrayList<Order>();
+			session.setAttribute("cart", cart);
+		}
+		
+		Order order = new Order();
+		order.setCount(count);
+		order.setMenuId(menu_id);
+		order.setTableNum(tableNum);
+		
+		cart.add(order);
+		
+		return "order_cart_ok.jsp";
+	}
+}
