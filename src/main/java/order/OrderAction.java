18f9d157acdd0d037b6242a97f3d9083b4d54721
nishimoto-maker <nishinish6121@gmail.com>
2026-07-13 10:14:19 +0900

    商品担当の方へ　ここに必要なメソッドを作ってください。お願いします。

--- src/main/java/order/OrderAction.java
+++ src/main/java/order/OrderAction.java
@@ -11,13 +11,29 @@ import tool.Action;
 public class OrderAction extends Action {
 	public String execute(HttpServletRequest req, HttpServletResponse resp)
 	throws Exception {
-		// ジャンルで絞り込んで取得する
-		// 指定しなければ全部取得
-		// 作ってもらうか、自分で作るか
-		MenuDAO dao = new MenuDAO();
-		List<Menu> list = dao.getMenu();
+		/*  ジャンルで絞り込んで取得する
+			指定しなければ全部取得
+			
+			order_top.jspに行く前に動くサーブレット
+			最初は何も指定しないので、全件取得・渡す
+			order_top.jspにアクセスした後、
+			ジャンルでソートできるように、
+			ジャンル一覧も一緒に送る
+		*/
+		// sortByで条件指定するメニュー取得と
+		// ジャンル一覧の取得をするメソッドを作ってほしいです
 		
-		req.setAttribute("menus", list);
+		String sortBy = req.getParameter("sort_by");
+		if (sortBy == null) {
+			sortBy = "";
+		}
+		
+		MenuDAO dao = new MenuDAO();
+		List<Menu> menus = dao.getMenu(sortBy);
+		List<String> ganres = dao.getGenre();
+		
+		req.setAttribute("menus", menus);
+		req.setAttribute("genres", ganres);
 		
 		return "order_top.jsp";
 	}
