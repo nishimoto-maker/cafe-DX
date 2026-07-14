fd8820ad1926d9f6018a1d831b8199221a253638
nishimoto-maker <nishinish6121@gmail.com>
2026-07-14 15:50:36 +0900

    これから作成していきます

---
+++ src/main/java/order/OrderConfirmAction.java
@@ -0,0 +1,40 @@
+/*セッションのカートから注文を取り出して、
+Orderに入れていき、List<Order>に格納していく
+それをDAOに渡して注文テーブルに登録する*/
+
+package order;
+
+import java.util.List;
+
+import bean.Order;
+import bean.OrderDetail;
+import dao.OrderDAO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+
+public class OrderConfirmAction extends Action {
+	@SuppressWarnings("unchecked")
+	public String execute(HttpServletRequest req, HttpServletResponse resp)
+	throws Exception {
+		HttpSession session = req.getSession();
+		
+		List<OrderDetail> cart = (List<OrderDetail>)session.getAttribute("cart");
+		
+		OrderDAO odao = new OrderDAO();
+		int line = 0;
+		
+		for (OrderDetail od : cart) {
+			Order o = new Order();
+			o.setTableNum(od.getTableNum());
+			o.setMenuId(od.getMenu().getMenu_id());
+			o.setCount(od.getCount());
+			
+			line += odao.insert(o);
+		}
+		
+		req.setAttribute("line", line);
+		return "order_confirm_ok.jsp";
+	}
+}
