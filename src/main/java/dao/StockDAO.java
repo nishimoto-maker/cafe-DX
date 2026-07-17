aaae8f2c29d35702e0de85c282012a99b2da08d4
nishimoto-maker <nishinish6121@gmail.com>
2026-07-17 17:55:43 +0900

    メソッド追加

--- src/main/java/dao/StockDAO.java
+++ src/main/java/dao/StockDAO.java
@@ -84,4 +84,23 @@ public class StockDAO extends DAO {
         }
         return null;
     }
+    
+    // 
+    public int getStock(String menuId) throws Exception {
+        String sql = "SELECT stock_count FROM stock WHERE menu_id = ?";
+        
+        try (Connection con = getConnection();
+             PreparedStatement st = con.prepareStatement(sql)) {
+            
+            st.setString(1, menuId);
+            
+            try (ResultSet rs = st.executeQuery()) {
+                if (rs.next()) {
+                	int stock = rs.getInt("stock_count");
+                	return stock;
+                }
+            }
+        }
+        return 0;
+    }
 }
